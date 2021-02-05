27abdd3a16f2b3d94aa8a78f7006f8cfe5ca09f3
LeeDarmon <leemickael.darmon@outlook.fr>
2021-02-05 17:38:59 +0100

    Update index_controller.php

--- controllers/index_controller.php
+++ controllers/index_controller.php
@@ -3,12 +3,12 @@
 $timestamp = time()+24*3600;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    setcookie('nom',$_POST['nom'],$timestamp);
-    setcookie('prenom',$_POST['prenom'],$timestamp);
-    setcookie('age',$_POST['age'],$timestamp);
-    setcookie('genre',$_POST['genre'],$timestamp);
-    setcookie('email',$_POST['email'],$timestamp);
-    setcookie('codepostal',$_POST['codepostal'],$timestamp);
-    setcookie('interet',$_POST['interet'],$timestamp);
+    setcookie('nom',$_POST['nom'],$timestamp, '/');
+    setcookie('prenom',$_POST['prenom'],$timestamp, '/');
+    setcookie('age',$_POST['age'],$timestamp, '/');
+    setcookie('genre',$_POST['genre'],$timestamp, '/');
+    setcookie('email',$_POST['email'],$timestamp, '/');
+    setcookie('codepostal',$_POST['codepostal'],$timestamp, '/');
+    setcookie('interet',$_POST['interet'],$timestamp, '/');
 
 }
